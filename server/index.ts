efdb25e8d3e3beac06f351abb3a2c12c45cf4ef2
afser husain basha <afsar.tanveer@gmail.com>
2026-03-14 16:41:00 +0530

    Remove Vite setup import from server index

--- server/index.ts
+++ server/index.ts
@@ -2,7 +2,6 @@ import express, { type Request, Response, NextFunction } from "express";
 import { registerRoutes } from "./routes";
 import { serveStatic } from "./static";
 import { createServer } from "http";
-import { setupVite } from "./vite.ts";
 
 const app = express();
 const httpServer = createServer(app);
